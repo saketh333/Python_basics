eed32624dc4a84667a94a1c5c15fe7869f96d1b7
Sai Saketh <saketh333@gmail.com>
2017-12-21 13:44:42 -0600

    Update regular_expressions.py

--- regular_expressions.py
+++ regular_expressions.py
@@ -1,10 +1,22 @@
 # regular expressions are available as standard library can be imported using re module
 import re 
 
-to_match = r"hello" #raw string
+to_match = r"hello" # raw string
 
-# match function checks if it matches at the beginning of the string takes two inputs
+# match function checks if the given string matches the beginning of the string
 if re.match(to_match, "hello"):
   print ("match")
 else:
   print("No match")
+
+# other regular expression functions re.search, re.findall
+if re.search(to_match, "hello"):
+  print ("match")
+else:
+  print("No match")
+
+if re.findAll(to_match, "hello"):
+  print ("match")
+else:
+  print("No match")
+
