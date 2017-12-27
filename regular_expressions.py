41122ba84c5f904f218fce71fd86a48e3601d834
Sai Saketh <saketh333@gmail.com>
2017-12-27 12:58:57 -0600

    Update regular_expressions.py

--- regular_expressions.py
+++ regular_expressions.py
@@ -4,27 +4,26 @@ import re
 to_match = r"hello" # raw string
 
 # match function checks if the given string matches the beginning of the string
-if re.match(to_match, "hello"):
+if re.match(to_match, r"heyhellohihola"):
   print ("match")
 else:
   print("No match")
 
-to_match = r"heyhellohihola"
-
 # other regular expression functions re.search, re.findall
 
 # search searches for that string in the given string  
-if re.search(to_match, "hello"):
+if re.search(to_match, r"heyhellohihola"):
   print ("match")
 else:
   print("No match")
 
 # finds and prints all the matched strings  
-if re.findall(to_match, "hello"):
+if re.findall(to_match, r"heyhellohihola"):
   print ("match")
 else:
   print("No match")
 
 # function re.finditer does the same thing as findall but returns an iterator
-iter =  re.finditer(to_match, "hello")
-print (iter)
+iter =  re.finditer(to_match, r"heyhellohihola")
+for i in iter:
+    print (i)
